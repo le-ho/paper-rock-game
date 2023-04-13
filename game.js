3aeeab75e4f525873deca30eba19455b70030008
le-ho <kliszcz@gmail.com>
2023-04-13 15:46:02 +0200

    added player choice and game results

--- game.js
+++ game.js
@@ -6,3 +6,26 @@ return computerElements = elements[Math.floor(Math.random() * elements.length)];
 
 }
 
+let playerElement = String(window.prompt("Choose paper,rock or scissors", ""));
+{
+if (playerElement.toLowerCase == "scissors" && computerElements == "paper") 
+return String("You Won! scissors beats paper");
+
+else if (playerElement.toLowerCase == "scissors" && computerElements == "rock")
+return String("You Lose! rock beats scissors");
+
+else if (playerElement.toLowerCase == "rock" && computerElements == "paper")
+return String("You Loose! paper beats rock");
+
+else if (playerElement.toLowerCase == "rock" && computerElements == "scissors")
+return String("You Won! rock beats scissors");
+
+else if (playerElement.toLocaleLowerCase == "paper" && computerElements == "rock")
+return String("You Won! paper beats rock");
+
+else if (playerElement.toLowerCase == "paper" && computerElements == "scissors")
+return String ("You Loose! Scissors beats paper");
+
+else if return String ("Draw");
+
+}
